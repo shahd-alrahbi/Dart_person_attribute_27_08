3c4331dc5083c02c322c16cab9420f66d7346d23
shahd-alrahbi <138566899+shahd-alrahbi@users.noreply.github.com>
2023-09-04 20:36:30 +0400

    Initial commit

---
+++ bin/person_attribute_27_08.dart
@@ -0,0 +1,31 @@
+void main() {}
+
+class Person {
+  String? name;
+  int? age;
+  String? email;
+
+  set setName(String name) {
+    name = name;
+  }
+
+  String? get getName {
+    return name;
+  }
+
+  set setage(int age) {
+    age = age;
+  }
+
+  int? get getage {
+    return age;
+  }
+
+  set setemail(String email) {
+    email = email;
+  }
+
+  String? get getemail {
+    return email;
+  }
+}
---
+++ test/person_attribute_27_08_test.dart
@@ -0,0 +1,8 @@
+import 'package:person_attribute_27_08/person_attribute_27_08.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
